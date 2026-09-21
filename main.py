1f2c6ca6ac615f324d6750ab70f0cc606cad0d56
abhishektw456 <abhishek.singh@thoughtwin.com>
2026-09-21 21:55:34 +0530

    get apis 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 
 app = FastAPI()
 
@@ -10,11 +10,17 @@ def index():
 def get_all_blogs():
     return {'message':'all blogs returned'}
 
-@app.get('/blog/{id}')
-def get_blog(id:int):
-    return {'message':f'blog with id {id}'}
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id:int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error':f'blog {id} not found'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message':f'blog with id {id}'}
 
 @app.get('/blog-with-params')
 # can pass default params
 def get_blog_params(page=1, data=10):
     return {'message':f' all {data} on page {page}'}
+# we can also combine path and query params in one api
